7332b15b200a6748449c96fcc16602b0acaf4a96
futuresfsake <chlseacllj@gmail.com>
2026-05-16 00:44:54 +0800

    added volume but the start of the gameplay would not work

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -13,3 +13,15 @@ func _on_resume():
 func _on_exit():
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/start_menu.tscn")  # change to your actual path
+
+
+func _on_volume_on_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_volume_mute_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
